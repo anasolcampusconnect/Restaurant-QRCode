15804f643c48e23232712026818dcdc17ede86c1
Akhil-K708 <akhilkodari708@gmail.com>
2026-05-27 12:50:49 +0530

    updated

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,32 +1,24 @@
 import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
-
-// Customer Pages
 import Menu from "../pages/customer/Menu";
 import Cart from "../pages/customer/Cart";
 import Checkout from "../pages/customer/Checkout";
 import OrderSuccess from "../pages/customer/OrderSuccess";
 import TrackOrder from "../pages/customer/TrackOrder";
 
-// Auth
 import Login from "../pages/auth/Login";
-
-// Admin Pages
 import Dashboard from "../pages/admin/Dashboard";
 import Orders from "../pages/admin/Orders";
 import MenuManagement from "../pages/admin/MenuManagement";
 import Tables from "../pages/admin/Tables";
 import Analytics from "../pages/admin/Analytics";
 
-// Kitchen Pages
 import KitchenDashboard from "../pages/kitchen/KitchenDashboard";
 import LiveOrders from "../pages/kitchen/LiveOrders";
 
-// Waiter Pages
 import WaiterDashboard from "../pages/waiter/WaiterDashboard";
 import ActiveTables from "../pages/waiter/ActiveTables";
 import Bills from "../pages/waiter/Bills";
 
-// Layouts
 import CustomerLayout from "../layouts/CustomerLayout";
 import AdminLayout from "../layouts/AdminLayout";
 import KitchenLayout from "../layouts/KitchenLayout";
@@ -37,11 +29,9 @@ function AppRoutes() {
     <HashRouter>
       <Routes>
         
-        {/* DEFAULT ROUTE: Redirect root to login for staff */}
         <Route path="/" element={<Navigate to="/login" replace />} />
 
         {/* CUSTOMER ROUTES */}
-        {/* QR scan chesthe direct ga ikkadiki vastaru, no login required */}
         <Route element={<CustomerLayout />}>
           <Route path="/table/:id" element={<Menu />} />
           <Route path="/cart" element={<Cart />} />
@@ -50,7 +40,6 @@ function AppRoutes() {
           <Route path="/track-order" element={<TrackOrder />} />
         </Route>
 
-        {/* LOGIN FOR STAFF */}
         <Route path="/login" element={<Login />} />
 
         {/* ADMIN ROUTES */}
